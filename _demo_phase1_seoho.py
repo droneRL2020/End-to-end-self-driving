5bcd5caf551cc145abe423254a0ba8c0f823a9d6
droneRL2020 <seoho0527@gmail.com>
2018-11-19 14:23:14 +0900

    straight line behavioral cloning

--- _demo_phase1_seoho.py
+++ _demo_phase1_seoho.py
@@ -5,6 +5,8 @@
 '''
 
 #!/usr/bin/env python
+import io
+#import motor
 import car_dir
 import RPi.GPIO as GPIO
 import PCA9685 as servo
@@ -13,12 +15,14 @@ import time
 import random
 import picamera
 from socket import *
-from time import ctime          # Import necessary modules
+from time import sleep, ctime          # Import necessary modules
 
 # Import behavioral_cloning
 import numpy as np
+from PIL import Image
 import tensorflow as tf
 import binarynet_classifier as bc
+
 
 num_classes = 1
 X_ph = tf.placeholder(tf.float32, [1, 64, 64, 3])
@@ -32,8 +36,7 @@ sess.run(init)
 restore_vars = [var for var in tf.global_variables() if
                var.name.startswith('binary_classifier')]
 saver = tf.train.Saver(restore_vars)
-saver.restore(sess, "anywhere/saved_networks2/2018-11-15_13_30")
-
+saver.restore(sess, "saved_networks3/2018-11-18_16_11")
 
 
 ctrl_cmd = ['left', 'right']
@@ -50,42 +53,46 @@ tcpSerSock.listen(5)
 
 car_dir.setup(busnum=busnum)
 
-current = time.time()
+now = time.time()
 car_dir.home()
-
+pwm = car_dir.setup()
 #model = behavioral_cloning.retModel()
+
+frames = 30000
+def capture():
+    frame = 0
+    stream = io.BytesIO()
+    prev = 400
+    while frame < frames:
+        yield stream
+        stream.seek(0)
+        frame += 1
+        img = Image.open(stream)
+        img = np.array(img)
+        img = np.expand_dims(img, axis=0)
+        hy_val = sess.run([hypothesis], feed_dict={X_ph: img})
+        hy_val = np.array(hy_val)
+        hy_val = np.clip(hy_val, -1, 1)
+        y_pred = (hy_val * 150) + 400 
+        current = int(y_pred)
+        print("prev pwm:", prev, "current pwm:",current)
+      
+        # PWM
+        car_dir.bc(prev, current)
+        prev = current
+        stream.seek(0)
+        stream.truncate()
 
 with picamera.PiCamera() as camera:
     camera.resolution = (64, 64)
-    camera.framerate = 24
-    #camera.start_preview()
+    camera.framerate = 30
     print('picamera warm up')
-    while True:
-        # capture image
-        #camera.capture(my_file)
-        img = np.empty((64,64,3), dtype=np.uint8)
-        camera.capture(img, 'rgb')
-        img = img.reshape((1,) + img.shape)
-        
-        # input image and get steer pwm
-        hy_val = sess.run([hypothesis], feed_dict={X_ph: img})
-        #y_pred = model.predict(img)
-        #y_pred = (((hy_val + 1) * 1023) / 2) + 987
-        y_pred = (hy_val * 450) +450
-        
-        after = time.time()
-        print(after - current)
-        # output steer pwm to servo motor real time
-        
-        #random_pwm = random.randint(450,550)
-        
-        print ("Waiting for connection...")
-        #tcpCliSock, addr = tcpSerSock.accept()
-        #print("...connected from:", addr)
-        #car_dir.turn_left()
-        #time.sleep(1)
-        #car_dir.turn_right()
-        #time.sleep(1)
-        car_dir.bc(y_pred)
-        print("hello world")
+    start = time.time()
+    camera.capture_sequence(capture(), use_video_port = True)
+         
+    print ("Waiting for connection...")
+    
+    camera.stop_preview()
+    now = time.time()
+    print('captured n images at %.2ffps' % (50 / (now - start)))
 tcpSerSock.close()
