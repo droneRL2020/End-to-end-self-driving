c68cb251dbb3d7d2dfec7d1615babc26b98ffeb2
droneRL2020 <seoho0527@gmail.com>
2019-03-07 04:14:45 -0500

    Check Servo's Minimum & Maximum PWM

---
+++ 0_servo_test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import PCA9685 as servo
+import time                  # Import necessary modules
+
+
+MinPulse = 250 # counter-clockwise max angle
+MaxPulse = 550 # clickwise max angle
+
+
+def setup():
+    global pwm
+    pwm = servo.PWM()
+    return pwm
+
+pwm = setup()
+def servo_test():
+    for value in range(MinPulse, MaxPulse):
+        pwm.write(0, 0, value)
+        #pwm.write(14, 0, value)
+       # pwm.write(15, 0, value)
+        print("hi")
+        #time.sleep(0.001)
+def servo_reverse():
+    for value in range(MaxPulse, MinPulse,-1):
+        pwm.write(0, 0, value)
+        print("hello world")
+        #time.sleep(0.001)
+if __name__ == '__main__':
+    #car_dir.turn(45)
+    #pwm.write(0, 0, MaxPulse) # This example is wrong example of pwm because it gives one pulse
+    #pwm.write(0, 0, MinPulse) # This example is wrong example of pwm becuase it gives one pulse
+    setup()
+    servo_test()
+    servo_reverse()
